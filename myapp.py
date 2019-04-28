e0bde7141b858af056073d83e0ac501e057b5527
Aadil Srivastava <aadil.srivastava01@gmail.com>
2019-04-28 10:08:46 +0530

    Added support for Postgres

--- myapp.py
+++ myapp.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +9,7 @@ from resources.myitems import Items,Item
 from resources.store import Store,Stores
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///mydb.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '#@$%1707@@#$1995'
 api = Api(app)
